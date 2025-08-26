3a83ed93de80b0c8e8495b8b89cc5c70459ad10c
philemon_dev <etoundelight1@gmail.com>
2025-08-26 18:42:32 +0100

    corr

--- src/app/core/services/vehicules/vehicules.service.ts
+++ src/app/core/services/vehicules/vehicules.service.ts
@@ -86,4 +86,12 @@ export class VehiculeService  {
     };
   }
 
+
+uploadCarteGrise(vehiculeId: number, formData: FormData): Observable<any> {
+  return this.http.post(`${this.apiUrl}/vehicules/${vehiculeId}/carte-grise`, formData);
+}
+
+
+
+
 }
