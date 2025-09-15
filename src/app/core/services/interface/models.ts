b045e50ad37beacdbe6b89dc2e5c3d63293d3304
Bassarou yacoubou <yacouboubassarou@gmail.com>
2025-09-15 16:30:05 +0100

    deploy

--- src/app/core/services/interface/models.ts
+++ src/app/core/services/interface/models.ts
@@ -241,7 +241,7 @@ export interface MouvementStock {
   created_at: string;
   updated_at: string;
   statut?: string;
-  demandevalidesigne?: string;
+  // demandevalidesigne?: string;
   unite_de_mesure:number;
   piece_jointe_mouvement?: string;
 
