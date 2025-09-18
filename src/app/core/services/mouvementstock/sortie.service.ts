656715ca56ddf204a95903c32377fcc0e4a62313
Bassarou yacoubou <yacouboubassarou@gmail.com>
2025-09-18 10:14:49 +0100

    console log mm s

--- src/app/core/services/mouvementstock/sortie.service.ts
+++ src/app/core/services/mouvementstock/sortie.service.ts
@@ -106,29 +106,12 @@ export class MouvementStockService  {
     return this.http.get(url, { responseType: 'blob' });
   }
 
-  // verifieStatus(code: string): Observable<Blob>  {
-  //   return this.http.get<any>(`${this.url}/mouvements/demande-sortie/check-status-and-generate/${code}`);
-  // }
-
 
   verifieStatus(code: string): Observable<Blob>  {
     // return this.http.get<any>(`${this.url}/mouvements/demande-sortie/check-status-and-generate/${code}`);
     return this.http.get(`${this.url}/mouvements/demande-sortie/check-status-and-generate/${code}`, { responseType: 'blob' });
   }
 
-
-//
-//   // Vérifie le statut uniquement (retour JSON attendu)
-//   verifieStatus2(code: string): Observable<any> {
-//     return this.http.get<any>(`${this.url}/mouvements/demande-sortie/check-status/${code}`);
-//   }
-//
-// // Génère et retourne le PDF groupé
-//   downloadGroupedFile2(code_mouvement: string): Observable<Blob> {
-//     const url = `${this.url}/mouvements/demande-sortie/check-status-and-generate/${encodeURIComponent(code_mouvement)}`;
-//     return this.http.get(url, { responseType: 'blob' });
-//   }
-//
 
 
 
